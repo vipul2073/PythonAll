5c33690ecf6a1e42cb36b73d97b475b42b152a8d
V <VPLNIGAM@GMAIL.COM>
2025-04-20 18:48:31 +0200

    Add files via upload

---
+++ Oddeven.py
@@ -0,0 +1,8 @@
+print("To check where you number is even of odd")
+numberq = type(input("enter a number"))
+print(numberq)
+number = int(input("enter a number"))
+if (number % 2) == 0:
+    print("You number is even")
+else:
+    print("Your number is odd")
